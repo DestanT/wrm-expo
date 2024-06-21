cf2ad93b0957a3e219ead8f880832df71678d9d3
Destan <destantezel@googlemail.com>
2024-06-21 22:23:45 +0100

    Add _layout file in (tabs) - redirect non session users to sign-in page

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,48 @@
+import { useEffect } from 'react';
+import { Redirect, Tabs } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+
+import { TabBarIcon } from '@/components/navigation/TabBarIcon';
+import { Colors } from '@/constants/Colors';
+import { useColorScheme } from '@/hooks/useColorScheme';
+import { useSession } from '@/contexts/AuthContext';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function TabLayout() {
+  const { session, isLoading } = useSession();
+  const colorScheme = useColorScheme();
+
+  useEffect(() => {
+    if (!isLoading) {
+      SplashScreen.hideAsync();
+    }
+  }, [isLoading]);
+
+  if (isLoading) {
+    return null;
+  }
+
+  if (!session) {
+    return <Redirect href='/sign-in' />;
+  }
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name='index'
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
