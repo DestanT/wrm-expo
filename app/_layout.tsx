58de49e6f44976ed19dbc68d5bf56839cc091493
Destan <destantezel@googlemail.com>
2024-06-21 18:27:26 +0100

    Add root _layout file and preload GestureHandler, Font, ThemeProvider for the rest of the app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,39 @@
+import { useCallback } from 'react';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Stack } from 'expo-router';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import 'react-native-reanimated';
+
+import { useColorScheme } from '@/hooks/useColorScheme';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const [fontsLoaded, fontError] = useFonts({
+    SpaceMono: require('@/assets/fonts/SpaceMono-Regular.ttf'),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded || fontError) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
-    <Stack>
-      <Stack.Screen name='index' />
-    </Stack>
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      <GestureHandlerRootView onLayout={onLayoutRootView}>
+        <Stack>
+          {/* <Stack.Screen name='index' options={{ headerShown: false }} /> */}
+          <Stack.Screen name='+not-found' />
+        </Stack>
+      </GestureHandlerRootView>
+    </ThemeProvider>
   );
 }
