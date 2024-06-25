c646f2d94ec7d431c459b9e9e0a666c4d70e8ba7
Destan <destantezel@googlemail.com>
2024-06-25 21:09:25 +0100

    Add IndieFlower font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout() {
   const colorScheme = useColorScheme();
   const [fontsLoaded, fontError] = useFonts({
     SpaceMono: require('@/assets/fonts/SpaceMono-Regular.ttf'),
+    IndieFlower: require('@/assets/fonts/IndieFlower.ttf'),
   });
 
   const onLayoutRootView = useCallback(async () => {
