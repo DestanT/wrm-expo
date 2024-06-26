5c4ac4e1f40dc0ee2f7e1fa1f04a052fc608fa97
Destan <destantezel@googlemail.com>
2024-06-26 12:54:59 +0100

    Add Pressable feature to each Cassette component - taking users to a detailed view of playlist

--- components/Cassette.tsx
+++ components/Cassette.tsx
@@ -1,23 +1,36 @@
-import { StyleSheet, ImageBackground, ViewStyle, Platform } from 'react-native';
+import { StyleSheet, ImageBackground, ViewStyle, Platform, Pressable } from 'react-native';
+import { useRouter } from 'expo-router';
 import NaturalText from './NaturalText';
 
 type CassetteProps = {
   title: string;
   style?: ViewStyle | null;
+  id: string | number; // NOTE: Update this once API is ready
 };
 
-export default function Cassette({ title, style }: CassetteProps) {
+export default function Cassette({ title, style, id }: CassetteProps) {
+  const router = useRouter();
+
+  const handlePress = () => {
+    router.push(`/playlist/${id}`);
+  };
+
   const imageStyle =
     Platform.OS === 'web' ? { ...styles.web, ...style } : { ...styles.mobile, ...style };
 
   return (
-    <ImageBackground
-      source={require('@/assets/images/cassette.png')}
-      style={imageStyle}
-      resizeMode='center'
+    <Pressable
+      onPress={handlePress}
+      style={({ pressed }) => [{ opacity: pressed ? 0.5 : 1 }, imageStyle]}
     >
-      <NaturalText text={title} />
-    </ImageBackground>
+      <ImageBackground
+        source={require('@/assets/images/cassette.png')}
+        style={imageStyle}
+        resizeMode='center'
+      >
+        <NaturalText text={title} />
+      </ImageBackground>
+    </Pressable>
   );
 }
 
